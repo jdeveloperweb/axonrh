68d76316dbc82ccf449a9c867ab74919123f1732
Jaime Vicente Jr <jdeveloperweb@yahoo.com.br>
2026-01-27 17:49:20 -0300

    feat: Adiciona a implementação inicial do Core Service AxonRH.

--- backend/core-service/src/main/java/com/axonrh/core/CoreServiceApplication.java
+++ backend/core-service/src/main/java/com/axonrh/core/CoreServiceApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.axonrh")
-@EnableJpaRepositories(basePackages = {"com.axonrh.core.repository", "com.axonrh.kafka.dlq"})
-@EntityScan(basePackages = {"com.axonrh.core.entity", "com.axonrh.kafka.dlq"})
+@EnableJpaRepositories(basePackages = {"com.axonrh.core.repository", "com.axonrh.core.setup.repository", "com.axonrh.kafka.dlq"})
+@EntityScan(basePackages = {"com.axonrh.core.entity", "com.axonrh.core.setup.entity", "com.axonrh.kafka.dlq"})
 public class CoreServiceApplication {
 
     public static void main(String[] args) {
