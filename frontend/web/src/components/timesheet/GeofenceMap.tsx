090a004fac63e1bd381c9bec7750237d9ea0fd57
Jaime Vicente Jr <jdeveloperweb@yahoo.com.br>
2026-01-29 23:48:59 -0300

    feat: Implementa a página de aprendizado de curso com visualizador de lições e progresso, e adiciona o componente ScrollArea.

--- frontend/web/src/components/timesheet/GeofenceMap.tsx
+++ frontend/web/src/components/timesheet/GeofenceMap.tsx
@@ -24,14 +24,14 @@ const userIcon = new L.Icon({
 });
 
 // Custom icon for geofence center
-const geofenceIcon = new L.Icon({
+/* const geofenceIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
-});
+}); */
 
 interface Geofence {
     id: string;
