ef13c623f669aaef4fe16338d4e6e9c45f95fe27
Jaime Vicente Jr <jdeveloperweb@yahoo.com.br>
2026-02-10 15:54:35 -0300

    fff

--- backend/timesheet-service/src/main/java/com/axonrh/timesheet/service/TimeAdjustmentService.java
+++ backend/timesheet-service/src/main/java/com/axonrh/timesheet/service/TimeAdjustmentService.java
@@ -30,10 +30,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 /**
